c210281e1b83bd80506a9c30c0ba0720e5df9be7
Lachlan Kalf <lachlankalf@gmail.com>
2022-07-30 21:40:32 +1000

    PBD Header Update

--- FileHandlers/MapEditor/PBDHandler.cs
+++ FileHandlers/MapEditor/PBDHandler.cs
@@ -13,23 +13,23 @@ namespace SSX_Modder.FileHandlers.MapEditor
         public byte[] MagicBytes;
         public int Unknown1;
         public int PatchCount;
-        public int UnknownCount1;
-        public int Unknown4;
+        public int InternalInstancesCount;
+        public int ParticleInstancesCount;
         public int Unknown5;
-        public int Unknown6;
-        public int Unknown7;
-        public int Unknown8;
+        public int ModelCount; //Model And Context Count Could be the wrong way around
+        public int LightCount;
+        public int SplinesCount;
         public int Unknown9;
         public int Unknown10;
-        public int Unknown11;
-        public int Unknown12;
+        public int ContextCount;
+        public int ParticleModelCount;
         public int Unknown13;
+
         public int Unknown14;
         public int Unknown15;
 
         public int Unknown17;
         public int PatchOffset;
-
         public int Unknown18;
         public int Unknown19;
         public int Unknown20;
@@ -58,16 +58,16 @@ namespace SSX_Modder.FileHandlers.MapEditor
                 MagicBytes = StreamUtil.ReadBytes(stream, 4);
                 Unknown1 = StreamUtil.ReadInt32(stream);
                 PatchCount = StreamUtil.ReadInt32(stream);
-                UnknownCount1 = StreamUtil.ReadInt32(stream);
-                Unknown4 = StreamUtil.ReadInt32(stream);
+                InternalInstancesCount = StreamUtil.ReadInt32(stream);
+                ParticleInstancesCount = StreamUtil.ReadInt32(stream);
                 Unknown5 = StreamUtil.ReadInt32(stream);
-                Unknown6 = StreamUtil.ReadInt32(stream);
-                Unknown7 = StreamUtil.ReadInt32(stream);
-                Unknown8 = StreamUtil.ReadInt32(stream);
+                ModelCount = StreamUtil.ReadInt32(stream);
+                LightCount = StreamUtil.ReadInt32(stream);
+                SplinesCount = StreamUtil.ReadInt32(stream);
                 Unknown9 = StreamUtil.ReadInt32(stream);
                 Unknown10 = StreamUtil.ReadInt32(stream);
-                Unknown11 = StreamUtil.ReadInt32(stream);
-                Unknown12 = StreamUtil.ReadInt32(stream);
+                ContextCount = StreamUtil.ReadInt32(stream);
+                ParticleModelCount = StreamUtil.ReadInt32(stream);
                 Unknown13 = StreamUtil.ReadInt32(stream);
                 Unknown14 = StreamUtil.ReadInt32(stream);
                 Unknown15 = StreamUtil.ReadInt32(stream);
@@ -96,24 +96,53 @@ namespace SSX_Modder.FileHandlers.MapEditor
                 Faces = new List<Face>();
                 for (int i = 0; i < PatchCount; i++)
                 {
-                    Face name = new Face();
-                    name.X = StreamUtil.ReadFloat(stream);
-                    name.Y = StreamUtil.ReadFloat(stream);
-                    name.Z = StreamUtil.ReadFloat(stream);
-                    name.vertex3s = new List<Vertex3>();
-                    stream.Position += 348;
+                    Face face = new Face();
+                    face.Points = new List<Vertex3>();
+                    for (int a = 0; a < 21; a++)
+                    {
+                        Vertex3 vertex3 = new Vertex3();
+                        vertex3.X = StreamUtil.ReadFloat(stream);
+                        vertex3.Y = StreamUtil.ReadFloat(stream);
+                        vertex3.Z = StreamUtil.ReadFloat(stream); 
+                        vertex3.W = StreamUtil.ReadFloat(stream);
+                        face.Points.Add(vertex3);
+                    }
+
+                    for (int a = 0; a < 2; a++)
+                    {
+                        Vertex3 vertex3 = new Vertex3();
+                        vertex3.X = StreamUtil.ReadFloat(stream);
+                        vertex3.Y = StreamUtil.ReadFloat(stream);
+                        vertex3.Z = StreamUtil.ReadFloat(stream);
+                        face.Points.Add(vertex3);
+                    }
+
+                    face.vertex3s = new List<Vertex3>();
                     for (int a = 0; a < 4; a++)
                     {
                         Vertex3 vertex3 = new Vertex3();
-                        vertex3.X = StreamUtil.ReadFloat(stream)/100f;
-                        vertex3.Y = StreamUtil.ReadFloat(stream)/100f;
-                        vertex3.Z = StreamUtil.ReadFloat(stream)/100f;
-                        vertex3.Unknown = StreamUtil.ReadFloat(stream);
-                        name.vertex3s.Add(vertex3);
+                        vertex3.X = StreamUtil.ReadFloat(stream);
+                        vertex3.Y = StreamUtil.ReadFloat(stream);
+                        vertex3.Z = StreamUtil.ReadFloat(stream);
+                        vertex3.W = StreamUtil.ReadFloat(stream);
+                        face.vertex3s.Add(vertex3);
                     }
-                    Faces.Add(name);
-                    stream.Position += 24;
+
+                    face.Unknown1 = StreamUtil.ReadInt32(stream); //Surface Type?
+                    face.Unknown2 = StreamUtil.ReadInt32(stream); //TrailS omething?
+
+                    //Counting Up Possible Thing for pathing?
+                    face.Unknown3 = StreamUtil.ReadInt32(stream);
+
+                    //Always the same
+                    face.Unknown4 = StreamUtil.ReadInt32(stream);
+                    face.Unknown5 = StreamUtil.ReadInt32(stream);
+                    face.Unknown6 = StreamUtil.ReadInt32(stream);
+
+                    Faces.Add(face);
+                   
                 }
+                Console.Beep();
             }
         }
 
@@ -125,12 +154,48 @@ namespace SSX_Modder.FileHandlers.MapEditor
 
     public struct Face
     {
-        public float X;
-        public float Y;
-        public float Z;
-
+        public List<Vertex3> Points;
         public List<Vertex3> vertex3s;
+
+        public int Unknown1;
+        public int Unknown2;
+        public int Unknown3;
+        public int Unknown4;//Negative one
+        public int Unknown5;
+        public int Unknown6;
     }
+
+
+
+//0 - Centre Point?/Start Point?
+
+//Possibly normal Makes a square
+
+//1 - All Basically 0,0,0
+//2 - All Basically 0,0,0
+//3 - All Basically 0,0,0
+//4 - All Basically 0,0,0
+
+
+    //5 - Unknown
+    //6 - Unknown
+    //7 - Unknown
+    //8 - Unknown
+
+    //9 - Unknown
+    //10 - Unknown
+    //11 - Unknown
+    //12 - Unknown
+
+    //13 - Unknown
+    //14 - Unknown
+    //15 - Unknown
+    //16 - Unknown
+
+    //17 - Unknown
+    //18 - Unknown
+    //19 - Unknown
+    //20 - Point on Model
 
     public struct Vertex3
     {
@@ -138,7 +203,7 @@ namespace SSX_Modder.FileHandlers.MapEditor
         public float Y;
         public float Z;
 
-        public float Unknown;
+        public float W;
     }
 
 }
